0ee62fe7c8ba6988e1c67f12c6dab46d4585bbae
luanlsr <luanfswd@gmail.com>
2021-09-26 00:27:47 -0300

    fix: :fire: fix contacts

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,55 +1,10 @@
-import React, { useState } from 'react';
-import {FaGithub, FaLinkedin} from 'react-icons/fa'
-import {GrInstagram} from 'react-icons/gr'
-import emailjs from 'emailjs-com';
-import '../styles/Contacts.css';
-
-const Result = () => {
-  return (
-    <p className="send-message">Sua mensagem foi enviada! Obrigado pelo contato.</p>
-  )
-}
+import React from 'react'
+import '../styles/Contacts.css'
 
 export default function Contact() {
-  const [sentEmail, setSendEmail] = useState(false)
-
-  function sendEmail(e) {
-    e.preventDefault();
-
-    emailjs.sendForm('portifolioEmail', 'template_dd1evh9', e.target, 'user_bbU9FFrioyGXwuHivDxRe')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset()
-      setSendEmail(true)
-  }
   return (
-    <div className="contactContainer" id="contacts">
-      <section className="contact-content">
-        <header>
-          <h1>Contato</h1>
-        </header>
-        <article className="social-networks">
-          <a href="https://github.com/luanlsr" target="__blank"><FaGithub className="github"/></a>
-          <a href="https://www.linkedin.com/in/luan-ramalholsr/" target="__blank"><FaLinkedin className="linkedin "/></a>
-          <a href="https://www.instagram.com/luan_ramalholsr/" target="__blank"><GrInstagram className="instagram"/></a>
-        </article>
-        <form className="contact-form" onSubmit={sendEmail}>
-          <input type="hidden" name="contact_number" />
-          <label className="label-name" >Nome</label>
-          <input className="name" type="text" name="user_name" />
-          <label className="label-email" >Email</label>
-          <input className="email" type="email" name="user_email" />
-          <label className="label-subject" >Assunto</label>
-          <input className="subject" type="text" name="subject" />
-          <label className="label-message" >Messagem</label>
-          <textarea className="message"  name="message" />
-          <input className="submit" type="submit" value="Enviar" />
-        </form>
-        {sentEmail ? <Result /> : null}
-      </section>
+    <div className="contactContainer">
+      Contacts
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,23 +11,15 @@ const NavBar = () => {
   return (
     <Navbar fixed="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="#about"><img src={logo} alt="logo" width="180px"/></Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
+      <Navbar.Brand href="#about"><img src={logo} alt="logo" width="180px"/></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav"/>
+      <Nav className="justify-content-end">
         <Nav.Link href="#about">About</Nav.Link>
         <Nav.Link href="#skills">Skills</Nav.Link>
         <Nav.Link href="#projects">Projects</Nav.Link>
-        {/* <NavDropdown title="Projects" id="collasible-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown> */}
         <Nav.Link href="#contacts">Contacts</Nav.Link>
       </Nav>
-    </Navbar.Collapse>
     </Container>
   </Navbar>
   )
